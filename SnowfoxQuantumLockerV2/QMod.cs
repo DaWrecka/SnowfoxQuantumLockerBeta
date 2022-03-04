5b7f7903988e132f3420656d10e42e60d1c941c6
DaWrecka <61927940+DaWrecka@users.noreply.github.com>
2022-03-04 18:03:53 +0000

    2022-03-04
    Fixed the Snowfox Quantum Locker setting, implemented in a separate project and with a new solution file

---
+++ SnowfoxQuantumLockerV2/QMod.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using Logger = QModManager.Utility.Logger;
+using SMLHelper.V2.Json;
+using SMLHelper.V2.Options.Attributes;
+using SMLHelper.V2.Handlers;
+using UnityEngine;
+
+namespace SnowFoxQuantumLocker
+{
+    [QModCore]
+    public static class QMod
+    {
+        public const string version = "1.0.1.0";
+
+        public static Config Config { get; } = OptionsPanelHandler.Main.RegisterModOptions<Config>();
+        [QModPatch]
+        public static void Patch()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var testMod = ($"Nagorogan_{assembly.GetName().Name}");
+            Logger.Log(Logger.Level.Info, $"Patching {testMod}");
+            Harmony harmony = new Harmony(testMod);
+            harmony.PatchAll(assembly);
+            Logger.Log(Logger.Level.Info, "Patched successfully!");
+        }
+    }
+    [Menu("SnowFox Quantum Locker")]
+    public class Config : ConfigFile
+    {
+        [Keybind("Quantum Locker Keybind", Tooltip = "When on the snowfox, press this key to open the built in quantum locker")]
+        public KeyCode LockerKey = KeyCode.C;
+        [Choice("Locker Type", new[] {"Standard", "Quantum", "Snowfox"}, Tooltip = "Decides what type of locker the snowfox will have. Standard is a standard locker, Quantum is a quantum locker that shares inventory with all other quantum lockers, Snowfox is a locker that shares inventory with all other snowfoxes")]
+        public string LockerType = "Standard";
+    }
+}
---
+++ SnowfoxQuantumLockerV2/SnowFoxQuantumLocker.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using Logger = QModManager.Utility.Logger;
+using SMLHelper.V2.Assets;
+using UWE;
+
+namespace SnowFoxQuantumLocker
+{
+    internal class SnowFoxQuantumLocker
+    {
+        [HarmonyPatch(typeof(Hoverbike))]
+        [HarmonyPatch("Update")]
+        internal class PatchHoverbikeUpdate
+        {
+            [HarmonyPostfix]
+
+            public static void Postfix(Hoverbike __instance)
+            {
+                if (__instance.GetPilotingCraft() && GameInput.GetKeyDown(QMod.Config.LockerKey))//GetPilotingCraft() just returns true if the player is currently riding the snowfox
+                {
+                    if (QMod.Config.LockerType.Equals("Standard"))
+                    {
+                        StorageContainer container = __instance.GetComponentInChildren<StorageContainer>(true);
+                        container.Open(container.transform);
+                    }
+                    else if (QMod.Config.LockerType.Equals("Quantum"))
+                    {
+                        var playerTransform = Player.main.gameObject?.transform;
+                        var QuantumStorage = QuantumLockerStorage.GetStorageContainer(false);
+                        if (playerTransform != null && QuantumStorage != null)
+                            QuantumStorage.Open(playerTransform);
+                    }
+                    else if(QMod.Config.LockerType.Equals("Snowfox"))
+                    {
+                        //ignore this
+                        //SnowfoxSharedStorage.storageContainer.Open(SnowfoxSharedStorage.storageContainer.transform);
+                    }
+                }
+            }
+        }
+        [HarmonyPatch(typeof (Hoverbike), nameof(Hoverbike.Awake))]
+        internal class PatchHoverbikeAwake
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Hoverbike __instance)
+            {
+                if(QMod.Config.LockerType.Equals("Standard"))
+                {
+                    //Current don't have a need to execute unique code under this condition, but I thought I probably would so I kept it
+                    //do stuff
+                    var storageRoot = __instance.transform.Find("StorageRoot")?.gameObject;//Tris to check if the snowfox already has a storage container on it from a previous play session
+                    StorageContainer storageContainer;
+                    if (storageRoot == null)
+                    {
+                        storageRoot = new GameObject("StorageRoot"); // make a new object
+                        storageRoot.transform.SetParent(__instance.transform); // set it a child of the parent            //these are metious' comments, obviously quite simplistic but it feels rude to delete them I guess
+                        storageRoot.SetActive(false); //this was because the storage container was trying to create itself before we could set its values and would cause a null ref exception
+
+                        //I don't even remember what half of this shit does, all I know is that me and mrpurple had no idea for a while and were just changing things between being on the snowfox and on the storageRoot
+                        //I still don't know which it's supposed to be on.
+                        var coi = __instance.gameObject.GetComponentInChildren<ChildObjectIdentifier>();
+                        if (coi == null)
+                        {
+                            Logger.Log(Logger.Level.Warn, "Oh Shit coi is null", null, true);
+                        }
+                        var pi = __instance.gameObject.GetComponent<PrefabIdentifier>();
+                        if (pi == null)
+                        {
+                            Logger.Log(Logger.Level.Warn, "Oh shit, pi is null", null, true);
+                        }
+                        else
+                        {
+                            coi.classId = pi.classId;
+                        }
+                        storageContainer = storageRoot.EnsureComponent<StorageContainer>();
+                        storageContainer.prefabRoot = __instance.gameObject;
+                        storageContainer.storageRoot = coi;
+
+                        storageContainer.width = 5;
+                        storageContainer.height = 5;
+                        storageContainer.storageLabel = "Snow Fox Storage";
+                        storageRoot.SetActive(true);
+                    }
+                    else
+                    {
+                        storageContainer = storageRoot.GetComponent<StorageContainer>();
+                    }
+
+                }
+                else if(QMod.Config.LockerType.Equals("Snowfox"))
+                {
+                    //haven't done this yet. not entirely sure how to
+                    //__instance.gameObject.AddComponent<SnowfoxSharedStorage>();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Hoverbike))]
+        internal class HoverbikeStartPatch
+        {
+            [HarmonyPatch(nameof(Hoverbike.Start))]
+            [HarmonyPostfix]
+            internal static void PostStart(Hoverbike __instance)
+            {
+                if (QMod.Config.LockerType.Equals("Quantum"))
+                {
+                    //var snowfoxQuantum = __instance.gameObject.EnsureComponent<SnowfoxQuantumStorage>();
+                    //snowfoxQuantum.storageContainer = storageContainer; // assign it the storage container you created earlier here
+                    __instance.gameObject.EnsureComponent<VehicleQuantumLockerComponent>();
+                }
+            }
+        }
+
+        public class VehicleQuantumLockerComponent : QuantumLocker
+        {
+            public void Awake()
+            {
+                CoroutineHost.StartCoroutine(DeferredInit());
+            }
+
+            private IEnumerator DeferredInit()
+            {
+                yield return new WaitForSecondsRealtime(0.1f);
+                QuantumLockerStorage.Register(this);
+            }
+
+            new public void Update() { }
+        }
+
+        //makes sure that this component's storagecontainer is always equal to the quantum locker main
+        class SnowfoxQuantumStorage : /*MonoBehaviour,*/ QuantumLocker, IProtoEventListener
+        {
+            //public StorageContainer storageContainer;
+
+            private bool synced;//what the fuck does this even do? We never set it to false and only set it to true at the end of the update method so when the fuck do we use it?
+            //private bool loadedFromSaveGame;
+
+            new void Update()
+            {
+                if (!synced)
+                {
+                    var quantumStorage = QuantumLockerStorage.GetStorageContainer(!loadedFromSaveGame); //I believe I could just do GetStorageContainer(true) but Lee yelled at me for that so I dunno
+                    if (quantumStorage != null)
+                    {
+                        storageContainer.SetContainer(quantumStorage.container);
+                        synced = true;
+                    }
+                }
+            }
+
+            /*public void OnProtoSerialize(ProtobufSerializer serializer)
+            {
+            }
+            */
+
+            public new void OnProtoDeserialize(ProtobufSerializer serializer)
+            {
+                QuantumLockerStorage.Register(this);
+                this.loadedFromSaveGame = true;
+            }
+        }
+        //unused currently. just ignore, don't feel like cleaning it up
+        internal class SharedStorage : Spawnable
+        {   
+            public SharedStorage() : base(classId: "SharedSnowfoxStorage", friendlyName: "Shared Snowfox Storage", description: "Random Description here")
+            {
+            }
+            public override GameObject GetGameObject()
+            {
+                var prefab = new GameObject();
+                prefab.SetActive(false);
+                prefab.AddComponent<LargeWorldEntity>().cellLevel = LargeWorldEntity.CellLevel.Global;
+                prefab.AddComponent<TechTag>().type = TechType;
+                prefab.AddComponent<PrefabIdentifier>().ClassId = ClassID;
+                prefab.AddComponent<MainStorage>();
+                return prefab;
+            }
+            /*public override List<SpawnLocation> CoordinatedSpawns => new List<Vector3> {new Vector3(425, 60, 55)};
+            CoordinatedSpawns = new List<Spawnable.SpawnLocation>()
+				{
+					new Spawnable.SpawnLocation(new Vector3(425, 60, 55), new Vector3(344f, 3.77f, 14f))
+				}*/
+        }
+        //used by SnowfoxQuantumStorage
+        class MainStorage : MonoBehaviour
+        {
+            public static MainStorage main;
+
+            private void Awake()
+            {
+                main = this;
+            }
+        }
+
+        /* This was me fucking around after mrpurple told me to try adding a storage container to an object that did not already have one so we(he) could see if it was the snowfox battery or upgrade modules storage fucking it over or not
+         *Still don't know, I never got this to work either. Pretty sure I could open it and put stuff in it but it would never save. idk
+        [HarmonyPatch(typeof(SolarPanel), nameof(SolarPanel.Start))]
+        public class SolarPanelPatch 
+        {
+            [HarmonyPrefix]
+            public static void Prefix(SolarPanel __instance)
+            {
+                var storageRoot = __instance.transform.Find("StorageRoot")?.gameObject;
+                StorageContainer storageContainer;
+                if(storageRoot == null)
+                {
+                    storageRoot = new GameObject("StorageRoot"); // make a new object
+                    storageRoot.transform.SetParent(__instance.transform); // set it a child of the parent
+                    storageRoot.transform.SetAsFirstSibling();
+                    storageRoot.SetActive(false); 
+                    var coi = storageRoot.EnsureComponent<ChildObjectIdentifier>();
+                    if(coi == null)
+                    {
+                        Logger.Log(Logger.Level.Warn,"Oh Shit coi is null", null, true);
+                    }
+                    var pi = __instance.gameObject.GetComponent<PrefabIdentifier>();
+                    if(pi == null)
+                    {
+                        Logger.Log(Logger.Level.Warn, "Oh shit, pi is null", null, true);
+                    }
+                    else
+                    {
+                        coi.classId = pi.classId;
+                    }
+                    storageContainer = storageRoot.EnsureComponent<StorageContainer>();
+                    storageContainer.prefabRoot = __instance.gameObject;
+                    storageContainer.storageRoot = coi;
+
+                
+                
+                    // this step is improtant for the game to recognize the storage as "saveable"
+
+                    storageContainer.width = 5;
+                    storageContainer.height = 5;
+                    storageContainer.storageLabel = "Snow Fox Storage";
+                    storageRoot.SetActive(true); 
+                }else
+                {
+                    storageContainer = storageRoot.GetComponent<StorageContainer>();
+                }
+            }
+        }
+        [HarmonyPatch(typeof(SolarPanel), nameof(SolarPanel.Update))]
+        public class SolarPanelUpdate 
+        {
+            [HarmonyPostfix]
+            public static void Postfix(SolarPanel __instance)
+            {
+                string summonKey = QMod.Config.LockerKey;
+                if(GameInput.GetKeyDown((KeyCode)summonKey.ToLower()[0]))
+                {
+                    StorageContainer Container = __instance.GetComponentInChildren<StorageContainer>();
+                    Container.Open(Container.transform);
+                }
+            }
+        }*/
+    }
+}
